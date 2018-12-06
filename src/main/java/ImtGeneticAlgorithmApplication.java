f212bde57dd4517986a618fcc95b3295da6dd125
OrPolyzos <orestes.polyzos@gmail.com>
2018-12-06 11:22:39 +0200

    refactored with providers

--- src/main/java/ImtGeneticAlgorithmApplication.java
+++ src/main/java/ImtGeneticAlgorithmApplication.java
@@ -1,12 +1,79 @@
+import com.algorithms.ai.domain.Chromosome;
 import com.algorithms.ai.exception.GeneticAlgorithmException;
-import imt.ga.ImtProblem;
+import imt.ga.domain.ImtGene;
+import imt.ga.domain.ImtGeneticAlgorithm;
+import imt.ga.domain.ImtKeyboard;
+
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class ImtGeneticAlgorithmApplication {
 
-    private static final String PHRASE = "to be or not to be that is the question";
+    private static final String TARGET_PHRASE = "to be or not to be that is the question";
+    private static final int POPULATION_COUNT = 1000;
+    private static final double MUTATION_RATE = 0.9;
 
     public static void main(String[] args) throws GeneticAlgorithmException {
-        new ImtProblem(PHRASE, 0.9, 1000).solve();
+        ImtGeneticAlgorithmApplication imtGeneticAlgorithmApplication = new ImtGeneticAlgorithmApplication();
+
+        ImtGeneticAlgorithm geneGeneticAlgorithm =
+                new ImtGeneticAlgorithm(MUTATION_RATE,
+                        imtGeneticAlgorithmApplication::generateInitialGeneration,
+                        imtGeneticAlgorithmApplication::calculateFitnessPerCorrectCharacter,
+                        imtGeneticAlgorithmApplication::selectPopulationFittestChromosome,
+                        imtGeneticAlgorithmApplication::pickCharactersAlternately,
+                        imtGeneticAlgorithmApplication::replaceCharacterAtRandomIndex,
+                        TARGET_PHRASE);
+        geneGeneticAlgorithm.run();
+    }
+
+    private List<Chromosome<ImtGene>> generateInitialGeneration() {
+        return IntStream.range(0, POPULATION_COUNT)
+                .boxed()
+                .map(i -> new Chromosome<>(new ImtGene(TARGET_PHRASE, ImtKeyboard.getRandomPhrase(TARGET_PHRASE.length()))))
+                .collect(Collectors.toList());
+    }
+
+    private double calculateFitnessPerCorrectCharacter(Chromosome<ImtGene> chromosomeToCalculateFitness) {
+        int correctCharactersCounter = 0;
+        String currentPhrase = chromosomeToCalculateFitness.getGene().getOwnPhrase();
+        for (int i = 0; i < currentPhrase.length(); i++) {
+            if (currentPhrase.charAt(i) == TARGET_PHRASE.charAt(i)) {
+                correctCharactersCounter++;
+            }
+        }
+        //Increase the power for a stronger fitness function for every correct character
+        return Math.pow(correctCharactersCounter, 1) / TARGET_PHRASE.length();
+    }
+
+    private Chromosome<ImtGene> selectPopulationFittestChromosome(List<Chromosome<ImtGene>> population, Chromosome<ImtGene> fittestChromosomeEver, Chromosome<ImtGene> fittestChromosome) {
+        return fittestChromosome;
+    }
+
+    private void replaceCharacterAtRandomIndex(Chromosome<ImtGene> chromosomeToMutate, double mutationRate) {
+        if (new Random().nextDouble() < mutationRate) {
+            String currentPhrase = chromosomeToMutate.getGene().getOwnPhrase();
+            StringBuilder mutatedPhraseBuilder = new StringBuilder(currentPhrase);
+            int chosenPosition = new Random().nextInt(currentPhrase.length());
+            mutatedPhraseBuilder.setCharAt(chosenPosition, ImtKeyboard.getRandomCharacterFromKeyboard());
+            chromosomeToMutate.getGene().setOwnPhrase(mutatedPhraseBuilder.toString());
+        }
+    }
+
+    private Chromosome<ImtGene> pickCharactersAlternately(Chromosome<ImtGene> firstParent, Chromosome<ImtGene> secondParent) {
+        StringBuilder newPhraseBuilder = new StringBuilder();
+        String firstParentPhrase = firstParent.getGene().getOwnPhrase();
+        String secondParentPhrase = secondParent.getGene().getOwnPhrase();
+        for (int i = 0; i < firstParentPhrase.length(); i++) {
+            if (i % 2 == 0) {
+                newPhraseBuilder.append(firstParentPhrase.charAt(i));
+            } else {
+                newPhraseBuilder.append(secondParentPhrase.charAt(i));
+            }
+        }
+        return new Chromosome<>(new ImtGene(TARGET_PHRASE, newPhraseBuilder.toString()));
     }
 }
 
--- src/main/java/imt/ga/domain/ImtGeneticAlgorithm.java
+++ src/main/java/imt/ga/domain/ImtGeneticAlgorithm.java
@@ -1,26 +1,24 @@
 package imt.ga.domain;
 
-import com.algorithms.ai.domain.Chromosome;
 import com.algorithms.ai.domain.GeneticAlgorithm;
 import com.algorithms.ai.provider.CrossOverTechniqueProvider;
 import com.algorithms.ai.provider.FitnessTechniqueProvider;
+import com.algorithms.ai.provider.InitialGenerationProvider;
 import com.algorithms.ai.provider.MutationTechniqueProvider;
 import com.algorithms.ai.provider.SelectionTechniqueProvider;
-
-import java.util.List;
 
 public class ImtGeneticAlgorithm extends GeneticAlgorithm<ImtGene> {
 
     private String targetPhrase;
 
     public ImtGeneticAlgorithm(double mutationRate,
-                               List<Chromosome<ImtGene>> initialGeneration,
+                               InitialGenerationProvider<ImtGene> initialGenerationProvider,
                                FitnessTechniqueProvider<ImtGene> fitnessTechniqueProvider,
                                SelectionTechniqueProvider<ImtGene> selectionTechniqueProvider,
                                CrossOverTechniqueProvider<ImtGene> crossOverTechniqueProvider,
                                MutationTechniqueProvider<ImtGene> mutationTechniqueProvider,
                                String targetPhrase) {
-        super(mutationRate, initialGeneration, fitnessTechniqueProvider, selectionTechniqueProvider, crossOverTechniqueProvider, mutationTechniqueProvider);
+        super(mutationRate, initialGenerationProvider, fitnessTechniqueProvider, selectionTechniqueProvider, crossOverTechniqueProvider, mutationTechniqueProvider);
         this.targetPhrase = targetPhrase;
     }
 
